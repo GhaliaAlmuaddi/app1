0f78ea04d7e95927ecefa3ece43fc2e208beb2f8
Ghalia Al Muaddi <ghaliaalmuaddi@gmail.com>
2023-10-05 12:04:26 +0300

    Initial Commit

---
+++ app1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  app1
+//
+//  Created by Ghalia Mohammed Al Muaddi on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app1/app1App.swift
@@ -0,0 +1,17 @@
+//
+//  app1App.swift
+//  app1
+//
+//  Created by Ghalia Mohammed Al Muaddi on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct app1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
